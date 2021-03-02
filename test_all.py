9c5cfbe19039b30ee596168710cbdb6e379ac51c
Steve Astels <sastels@gmail.com>
2021-03-02 11:50:09 -0500

    fix ci

--- test_all.py
+++ test_all.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-for dir in os.listdir():
+for dir in os.listdir('.'):
     if os.path.isdir(dir) and not dir.startswith('.'):
         retval = os.system("cd {} && go test".format(dir)) != 0
         if retval != 0:
