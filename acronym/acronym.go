23b26dc68b45c83f2cc8c4daf00c673ac798c461
Steve Astels <sastels@gmail.com>
2021-03-02 12:26:48 -0500

    done

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,15 +1,21 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package acronym
 
-// Abbreviate should have a comment documenting it.
+import (
+	"regexp"
+	"strings"
+)
+
+// Abbreviate makes an acronym
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	matches := regexp.MustCompile(`([-_])`)
+	s = matches.ReplaceAllString(s, " ")
+
+	matches = regexp.MustCompile(`(\s+)`)
+	s = matches.ReplaceAllString(s, " ")
+
+	acc := ""
+	for _, value := range strings.Split(s, " ") {
+		acc += strings.ToUpper(value[:1])
+	}
+	return acc
 }
