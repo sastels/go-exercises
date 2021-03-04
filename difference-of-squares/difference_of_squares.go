a60e1c751a44e62b22ae2ddb755da2160a63891b
Steve Astels <sastels@gmail.com>
2021-03-04 12:55:42 -0500

    init

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,16 @@
+package diffsquares
+
+// SquareOfSum squares the sum of the first n integers
+func SquareOfSum(n int) int {
+	return 0
+}
+
+// SumOfSquares sums the squares of the first n integers
+func SumOfSquares(n int) int {
+	return 0
+}
+
+// Difference subtracts the two
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
