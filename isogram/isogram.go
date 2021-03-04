f29ae23aee8df0ed93b41aae1242a43faf3fddaa
Steve Astels <sastels@gmail.com>
2021-03-04 12:32:23 -0500

    done

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,6 +1,20 @@
 package isogram
 
+import (
+	"regexp"
+	"strings"
+)
+
 // IsIsogram checks if a string has a repeated letter
 func IsIsogram(s string) bool {
-	return false
+	matches := regexp.MustCompile(`([- ])`)
+	s = matches.ReplaceAllString(s, "")
+	s = strings.ToUpper(s)
+
+	for i, c := range s {
+		if strings.ContainsRune(s[:i], c) {
+			return false
+		}
+	}
+	return true
 }
