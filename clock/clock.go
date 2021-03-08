e69054b297445933ef66756d00ef22e20751ef75
Steve Astels <sastels@gmail.com>
2021-03-08 13:34:47 -0500

    rough in

--- clock/clock.go
+++ clock/clock.go
@@ -1,1 +1,25 @@
 package clock
+
+// Clock represents a clock
+type Clock struct {
+	hour, min int
+}
+
+// New returns a new clock
+func New(hour, min int) Clock {
+	return Clock{hour, min}
+}
+
+// Add adds minutes to a clock
+func (c Clock) Add(min int) Clock {
+	return c
+}
+
+// Subtract subtracts minutes from a clock
+func (c Clock) Subtract(min int) Clock {
+	return c
+}
+
+func (c Clock) String() string {
+	return ""
+}
