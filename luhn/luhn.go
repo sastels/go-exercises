c626f5a4c5a0bacb7d00504727c65b2b7beeb248
Steve Astels <sastels@gmail.com>
2021-03-08 12:14:31 -0500

    init

---
+++ luhn/luhn.go
@@ -0,0 +1,13 @@
+package luhn
+
+import "strings"
+
+// Valid checks if s is a Luan number
+func Valid(s string) bool {
+	s = strings.ReplaceAll(s, " ", "")
+
+	// for _, c := range s {
+
+	// }
+	return false
+}
