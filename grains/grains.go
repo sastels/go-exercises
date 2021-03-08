97fdc982c57a25ee9b2e9faf84a06eafb0e735ce
Steve Astels <sastels@gmail.com>
2021-03-08 12:58:44 -0500

    init

---
+++ grains/grains.go
@@ -0,0 +1,13 @@
+package grains
+
+// Square returns the number of grains on square n
+func Square(n int) (uint64, error) {
+
+	return 0, nil
+}
+
+// Total returns the number of grains on the board
+func Total() uint64 {
+
+	return 0
+}
