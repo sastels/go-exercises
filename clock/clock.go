b2445713f82fc3215fb4d127ce151c41c391c1d3
Steve Astels <sastels@gmail.com>
2021-03-08 13:51:50 -0500

    done

--- clock/clock.go
+++ clock/clock.go
@@ -1,5 +1,7 @@
 package clock
 
+import "fmt"
+
 // Clock represents a clock
 type Clock struct {
 	hour, min int
@@ -7,19 +9,32 @@ type Clock struct {
 
 // New returns a new clock
 func New(hour, min int) Clock {
+	if min < 0 {
+		hour -= 1 + (-min / 60)
+		min += 60 + 60*(-min/60)
+	}
+	hour += (min / 60)
+	min = min % 60
+	if hour < 0 {
+		hour += 24 + 24*(-hour/24)
+	}
+	hour = hour % 24
 	return Clock{hour, min}
 }
 
 // Add adds minutes to a clock
 func (c Clock) Add(min int) Clock {
-	return c
+	return New(c.hour, c.min+min)
+
 }
 
 // Subtract subtracts minutes from a clock
 func (c Clock) Subtract(min int) Clock {
-	return c
+	return New(c.hour, c.min-min)
 }
 
+// String returns string representation of a clock
 func (c Clock) String() string {
-	return ""
+	return fmt.Sprintf("%02d:%02d", c.hour, c.min)
+
 }
