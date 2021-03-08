be2b5f485c36997ad26451c7cc3cb768c317aed1
Steve Astels <sastels@gmail.com>
2021-03-08 13:09:15 -0500

    done

--- grains/grains.go
+++ grains/grains.go
@@ -1,13 +1,21 @@
 package grains
 
+import "errors"
+
 // Square returns the number of grains on square n
 func Square(n int) (uint64, error) {
-
-	return 0, nil
+	if n <= 0 || n > 64 {
+		return 0, errors.New("bad value")
+	}
+	return 1 << (n - 1), nil
 }
 
 // Total returns the number of grains on the board
 func Total() uint64 {
-
-	return 0
+	sum := uint64(0)
+	for i := 1; i < 65; i++ {
+		n, _ := Square(i)
+		sum += n
+	}
+	return sum
 }
