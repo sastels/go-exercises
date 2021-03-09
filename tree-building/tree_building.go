30fa006b3f7214a77abc91fd4af5224602592b11
Steve Astels <sastels@users.noreply.github.com>
2021-03-09 15:40:18 -0500

    Tree building (#17)

---
+++ tree-building/tree_building.go
@@ -0,0 +1,64 @@
+package tree
+
+import (
+	"errors"
+	"sort"
+)
+
+// Record stores the ids of the tree nodes
+type Record struct {
+	ID, Parent int
+}
+
+// Node is a node in the tree
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+// Build builds a tree
+func Build(input []Record) (*Node, error) {
+	if len(input) == 0 {
+		return nil, nil
+	}
+
+	nodes := make([]Node, len(input))
+	var root *Node = nil
+
+	for i := 0; i < len(input); i++ {
+		nodes[i] = Node{ID: i}
+	}
+
+	IDsSeen := map[int]bool{}
+	for _, record := range input {
+		ID := record.ID
+		parent := record.Parent
+		if IDsSeen[ID] {
+			return nil, errors.New("Duplicate ID")
+		}
+		if ID < 0 || ID >= len(input) || parent < 0 || parent >= len(input) {
+			return nil, errors.New("Bad ID")
+		}
+		if parent > ID {
+			return nil, errors.New("Bad parent")
+		}
+		IDsSeen[ID] = true
+		if ID == parent {
+			if root != nil {
+				return nil, errors.New("duplicate root")
+			}
+			root = &nodes[ID]
+		} else {
+			children := append(nodes[parent].Children, &(nodes[ID]))
+			sort.SliceStable(children, func(i, j int) bool {
+				return children[i].ID < children[j].ID
+			})
+			nodes[parent].Children = children
+		}
+	}
+	if root == nil {
+		return nil, errors.New("no root")
+	}
+
+	return root, nil
+}
