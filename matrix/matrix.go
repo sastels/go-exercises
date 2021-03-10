ff54fbd5d68199102e1f2abd3cf9905fb4b53f50
Steve Astels <sastels@gmail.com>
2021-03-10 15:27:45 -0500

    rough in a bit

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,1 +1,27 @@
 package matrix
+
+// Matrix represents a matrix
+type Matrix struct {
+	data             []int
+	numRows, numCols int
+}
+
+// New returns a new matrix
+func New(input string) (*Matrix, error) {
+	return nil, nil
+}
+
+// Set sets part of the matrix
+func (m *Matrix) Set(x, y, val int) bool {
+	return false
+}
+
+// Rows returns the rows of the matrix
+func (m Matrix) Rows() [][]int {
+	return [][]int{}
+}
+
+// Cols returns the columns of the matrix
+func (m Matrix) Cols() [][]int {
+	return [][]int{}
+}
