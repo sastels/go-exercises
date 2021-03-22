70d2cf57a33ed1c22e5b76a0a92ba46cd0e28a54
Steve Astels <sastels@gmail.com>
2021-03-22 16:53:37 -0400

    TestReadTotalReader passing

--- paasio/paasio.go
+++ paasio/paasio.go
@@ -2,9 +2,11 @@ package paasio
 
 import (
 	"io"
+	"sync"
 )
 
 type PaasReader struct {
+	mutex  *sync.Mutex
 	reader io.Reader
 	n      *int64
 	nops   *int
@@ -15,14 +17,16 @@ func (a PaasReader) ReadCount() (int64, int) {
 }
 
 func (a PaasReader) Read(p []byte) (int, error) {
+	a.mutex.Lock()
 	n, err := a.reader.Read(p)
 	*a.n += int64(n)
 	*a.nops++
+	a.mutex.Unlock()
 	return n, err
 }
 
 func NewReadCounter(reader io.Reader) ReadCounter {
 	n := int64(0)
 	nops := 0
-	return PaasReader{reader, &n, &nops}
+	return PaasReader{&(sync.Mutex{}), reader, &n, &nops}
 }
